82df123c4da12b89bb905501a9486ac8fc7a6d50
Bobbaru <bobbaru_krueger@yahoo.com>
2020-12-22 15:47:45 +0200

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,8 @@ const files = {
     svgsimgPathTo   : 'dist/imgs/',
     gifsimgPath     : 'src/imgs/**/*.gif',
     gifsimgPathTo   : 'dist/imgs/',
+    webpsimgPath    : 'src/imgs/**/*.webp',
+    webpsimgPathTo  : 'dist/imgs/',
     html_r          : 'site.html'
 }
 
@@ -136,14 +138,6 @@ function deleteTask() {
         .pipe(clean())
 }
 
-// Edit index.html - imgs src
-function editHTML() {
-    return src(['index.html'])
-        .pipe(replace('src/', 'dist/'))
-        // .pipe(replace('<img src="', '<img data-src="')) // 4 lazyload
-        .pipe(dest('.'))
-}
-
 // Cachebust
 var cbString = new Date().getTime();
 function cacheBustTask(){
@@ -152,7 +146,7 @@ function cacheBustTask(){
         .pipe(dest('.'));
 }
 
-// Copy svg and gifs imgs bc tinypng cannot optimize them
+// Copy svg, gifs and webp imgs bc tinypng cannot optimize them
 function copysvgs() {
     return src(files.svgsimgPath)
         .pipe(dest(files.svgsimgPathTo))
@@ -160,6 +154,10 @@ function copysvgs() {
 function copygifs() {
     return src(files.gifsimgPath)
         .pipe(dest(files.gifsimgPathTo))
+}
+function copywebp() {
+    return src(files.webpsimgPath)
+        .pipe(dest(files.webpsimgPathTo))
 }
 
 // Minify Img
@@ -189,14 +187,6 @@ function editHTML() {
         .pipe(dest('.'))
 }
 
-// Cachebust
-var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
-}
-
 // Edit csseco-style.css - imgs src
 function editCSS() {
     return src(['dist/css/*.css'])
@@ -221,8 +211,6 @@ function watchTask(){
 
 // When the site is ready to deploy
 exports.deep = series(
-    copysvgs,
-    copygifs,
     jquery,
     popperjs,
     scssTask, 
@@ -242,11 +230,17 @@ exports.deep = series(
 
 // Minify via Tinypng
 exports.tiny = series(
+    copysvgs,
+    copygifs,
+    copywebp,
     minImg,
-)
-
+    )
+    
 // Minify via webp
 exports.webp = series(
+    copysvgs,
+    copygifs,
+    copywebp,
     webpimgs,
 )
 
