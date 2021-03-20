341b681496b6d348d3b6aa00a18fc2c7c84cea8f
Bobbaru <bobbaru_krueger@yahoo.com>
2021-03-20 23:11:20 +0200

    add ie support :))))

--- src/js/concat/02-gsap.js
+++ src/js/concat/02-gsap.js
@@ -1,52 +1,39 @@
 /*! GSAP related scripts */
 
 //------------------------------------
-// GSAP Register Plugins
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-
-
-
-
-//------------------------------------
 // GSAP ScrollTo
 
 let scsseco_scrollTo = document.querySelectorAll(".scsseco_scrollTo");
 
 scsseco_scrollTo.forEach(function (el, index) {
-  el.addEventListener("click", function(e) {
+  el.addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     gsap.to(window, {
-      duration: 1, 
-      scrollTo: el.getAttribute("href")
+      duration: 1,
+      scrollTo: el.getAttribute("href"),
     });
   });
 });
-
-
-
-
 
 //------------------------------------
 // GSAP Animations
 const patrat = document.querySelector(".patrat");
 
 const tl = gsap.timeline({
-  defaults: { // children inherit these defaults
+  defaults: {
+    // children inherit these defaults
     duration: 1,
-    ease: "linear" 
-  }
+    ease: "linear",
+  },
 });
 
-tl
-  .to(patrat, {
-    duration: 1,
-    x: 300,
-    y: -260,
-    rotate: 360,
-  })
+tl.to(patrat, {
+  duration: 1,
+  x: 300,
+  y: -260,
+  rotate: 360,
+});
 
 ScrollTrigger.create({
   animation: tl,
---
+++ src/js/concat/03-misc.js
@@ -0,0 +1,14 @@
+/*!
+ * Misc scripts
+ */
+(function () {
+  /*! JQuery scripts */
+
+  //------------------------------------
+  // Add Classes to img element
+  $("img").addClass("img-fluid lazyload");
+
+  //------------------------------------
+  // Add Classes to tabel element
+  $("table").addClass("table table-bordered");
+})();
